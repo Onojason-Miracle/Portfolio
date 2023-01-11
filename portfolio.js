37fff19f83bcee3877fe531a58ce802361e1cf93
Onojason Miracle Ojonuyo <onojasonmiracle@gmail.com>
2023-01-11 03:06:58 +0100

    responsiveness and other changes

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,10 @@
 // function for slidess
 
+const d = new Date();
+const hello = d.getFullYear();
+
+document.querySelector(".footer-date")
+.innerHTML = hello;
 let mySlides = 0;
 showSlides();
 
@@ -48,45 +53,10 @@ function projectSlides() {
 
 
 
-let now = new Date().toJSON();
-let mimie = Date.now();
-const date = new Date();
 
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
 
-// This arrangement can be altered based on how we want the date's format to appear.
-let currentDate = `${day}-${month}-${year}`;
-let current_date =
-  date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
-// document.querySelector("#date").innerHTML = current_date;
 
-// function for hamburger
-//    function myFunction() {
-//     let nav  = document.getElementById("navbar");
-//     let hamburger  = document.querySelector(".hamburger");
-//     if (nav.style.display === "none") {
-//       hamburger.style.display = "block";
-//     } else {
-//       x.style.display = "none";
-//     }
+// const d = new Date();
+// const hello = d.getFullYear();
 
-function myFunction() {
-  let hamburger = document.getElementById("navbar");
-  // if (x.className.indexOf("w3-show") == -1) {
-  //   x.className += " w3-show";
-  // } else {
-  //   x.className = x.className.replace(" w3-show", "");
-  // }
-}
-
-//   let icons = document.querySelector(".skills_icons");
-
-//   function iconia(){
-//    let x = document.createElement("p");
-//    x = `<p class="text-center text-light">This is Js oo</p>`
-// icons.appendChild(x);
-//   }
-
-//   icons.addEventListener("mouseover", iconia)
+// document.getElementByClass("footer-date").innerHTML = d.getFullYear();
